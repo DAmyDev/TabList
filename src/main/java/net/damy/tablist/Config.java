8254efffa04e54bbe7047799da6da214feb58e44
DAmy <teeion345@gmail.com>
2026-03-16 17:57:03 +0700

    0.0.0

---
+++ src/main/java/net/damy/tablist/Config.java
@@ -0,0 +1,8 @@
+package net.damy.tablist;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Tablist.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config {
+
+}
---
+++ src/main/java/net/damy/tablist/Tablist.java
@@ -0,0 +1,16 @@
+package net.damy.tablist;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(Tablist.MODID)
+public class Tablist {
+
+    public static final String MODID = "tablist";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Tablist() {
+
+    }
+}
